30552b7fb84b5b4abbdf88a0d3df67567cf1b9de
Mimoune Louarradi <mlouarra@gmail.com>
2024-03-24 18:29:45 +0100

    Description des modifications

---
+++ dags/run_chicago_crime_dag.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+# Fonction wrapper pour votre script principal
+def run_main():
+    import sys
+    sys.path.insert(0, '/opt/airflow/Chicago_Crime')  # Ajoutez le chemin du projet
+    from main import main  # Importez la fonction main depuis le fichier main.py
+    main()  # Appelez la fonction principale de votre projet
+
+
+
+# Arguments par défaut pour le DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),  # Assurez-vous que cette date est dans le passé
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Définir le DAG
+dag = DAG(
+    'chicago_crime_dag',
+    default_args=default_args,
+    description='Run Chicago Crime main.py',
+    schedule_interval='*/120 * * * *',
+    catchup=False,
+)
+
+# Définir la tâche qui exécute votre script principal
+run_task = PythonOperator(
+    task_id='run_main',
+    python_callable=run_main,
+    dag=dag,
+)
+
+# Ordonnancer la tâche
+run_task
